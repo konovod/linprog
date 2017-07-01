d0a778a7e62f90bd35a706b8a7a42e27863a570f
Konovod <konovod@gmail.com>
2017-07-01 19:19:06 +0000

    vars sonstraints (untested)

--- src/solver/symphony.cr
+++ src/solver/symphony.cr
@@ -11,15 +11,37 @@ module Symphony
     Maximize = -1
   end
 
+  record Constraint, min : Float64, max : Float64, integer : Bool do
+    def self.none
+      new(Float64::MIN, Float64::MAX, false)
+    end
+
+    def self.positive
+      new(0.0, Float64::MAX, false)
+    end
+
+    def self.negative
+      new(Float64::MIN, 0.0, false)
+    end
+
+    def self.integer
+      new(-Int64::MIN, -Int64::MAX, true)
+    end
+
+    def self.binary
+      new(0.0, 1.0, true)
+    end
+  end
+
   class Problem
     getter nrows : Int32
     getter ncolumns : Int32
     getter sparse_starts
     getter sparse_indices
     getter sparse_values
-    getter collb
-    getter colub
-    getter is_int
+    getter collb : Slice(Float64)
+    getter colub : Slice(Float64)
+    getter is_int : Slice(UInt8)
     getter obj
     getter obj2
     getter rowsen
@@ -27,7 +49,7 @@ module Symphony
     getter rowrng
     getter correct = false
 
-    def self.from_dense(*, c, a_ub, b_ub, a_eq, b_eq)
+    def self.from_dense(*, c, a_ub, b_ub, a_eq, b_eq, bounds : (Constraint | Indexable(Constraint) | Nil) = nil)
       raise ArgumentError.new("Columns count of a_ub and a_eq must match") if a_ub.ncolumns != a_eq.ncolumns
       raise ArgumentError.new("Rows count of b_ub and a_ub must match") if a_ub.nrows != b_ub.nrows
       raise ArgumentError.new("Rows count of b_eq and a_eq must match") if a_eq.nrows != b_eq.nrows
@@ -35,10 +57,13 @@ module Symphony
       raise ArgumentError.new("b_ub must have single column") if b_ub.ncolumns != 1
       raise ArgumentError.new("b_eq must have single column") if b_eq.ncolumns != 1
       raise ArgumentError.new("c must have single row") if c.nrows != 1
-      new(c: c, a_ub: a_ub, b_ub: b_ub, a_eq: a_eq, b_eq: b_eq)
+      if bounds.is_a?(Indexable(Constraint)) && c.nrows != bounds.size
+        raise ArgumentError.new("bounds.size must match columns of c")
+      end
+      new(c: c, a_ub: a_ub, b_ub: b_ub, a_eq: a_eq, b_eq: b_eq, bounds: bounds)
     end
 
-    protected def initialize(*, c, a_ub, b_ub, a_eq, b_eq)
+    protected def initialize(*, c, a_ub, b_ub, a_eq, b_eq, bounds)
       @nrows = a_ub.nrows + a_eq.nrows
       @ncolumns = a_ub.ncolumns
       @sparse_starts = Slice(Int32).new(@ncolumns + 1) { |i| i*@nrows }
@@ -48,9 +73,20 @@ module Symphony
         column = i / @nrows
         Float64.new(row < a_ub.nrows ? a_ub[row, column] : a_eq[row, column])
       end
-      @collb = Slice(Float64).new(@ncolumns, 0.0)
-      @colub = Slice(Float64).new(@ncolumns, Float64::INFINITY)
-      @is_int = Slice(UInt8).new(@ncolumns, 0u8) # false
+      case bounds
+      when Nil
+        @collb = Slice(Float64).new(@ncolumns, 0.0)
+        @colub = Slice(Float64).new(@ncolumns, Float64::MAX)
+        @is_int = Slice(UInt8).new(@ncolumns, 0u8) # false
+      when Constraint
+        @collb = Slice(Float64).new(@ncolumns, bounds.min)
+        @colub = Slice(Float64).new(@ncolumns, bounds.max)
+        @is_int = Slice(UInt8).new(@ncolumns, bounds.integer ? 0u8 : 1u8)
+      else
+        @collb = Slice(Float64).new(@ncolumns) { |i| bounds[i].min.as(Float64) }
+        @colub = Slice(Float64).new(@ncolumns) { |i| bounds[i].max.as(Float64) }
+        @is_int = Slice(UInt8).new(@ncolumns) { |i| bounds[i].integer ? 0u8 : 1u8 }
+      end
       @obj = Slice(Float64).new(@ncolumns) { |i| Float64.new(c[0, i]) }
       @obj2 = Slice(Float64).new(@ncolumns, 0.0)
       @rowsen = Slice(UInt8).new(@nrows) { |i| UInt8.new(i < a_ub.nrows ? 'L'.ord : 'E'.ord) }
@@ -59,18 +95,18 @@ module Symphony
       @correct = true
     end
 
-    def self.from_dense(*, c, a_eq, b_eq)
+    def self.from_dense(*, c, a_eq, b_eq, bounds = nil)
       from_dense(c: c,
         a_ub: Linalg::Mat.zeros(0, a_eq.ncolumns),
         b_ub: Linalg::Mat.zeros(0, 1),
-        a_eq: a_eq, b_eq: b_eq)
+        a_eq: a_eq, b_eq: b_eq, bounds: bounds)
     end
 
-    def self.from_dense(*, c, a_ub, b_ub)
+    def self.from_dense(*, c, a_ub, b_ub, bounds = nil)
       from_dense(c: c,
         a_eq: Linalg::Mat.zeros(0, a_ub.ncolumns),
         b_eq: Linalg::Mat.zeros(0, 1),
-        a_ub: a_ub, b_ub: b_ub)
+        a_ub: a_ub, b_ub: b_ub, bounds: bounds)
     end
   end
 
